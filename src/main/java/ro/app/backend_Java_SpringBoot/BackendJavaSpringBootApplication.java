74b5487775b42ef59f37360d6128f70d90a9b521
TurcanuTeodor <turcanuteodor23@stud.ase.ro>
2025-11-30 18:26:28 +0200

    add cache config

--- src/main/java/ro/app/backend_Java_SpringBoot/BackendJavaSpringBootApplication.java
+++ src/main/java/ro/app/backend_Java_SpringBoot/BackendJavaSpringBootApplication.java
@@ -2,7 +2,9 @@ package ro.app.backend_Java_SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BackendJavaSpringBootApplication {
 
